b7f55db195f771045efdfe0c6256e8a26485555d
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-04 18:33:31 +0200

    create locations router

--- routes/locations.js
+++ routes/locations.js
@@ -2,3 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const Locations = require("../models/Character");
+
+router.get("/locations", (req, res) => {
+  res.json({ message: "Locations List" });
+});
+
+module.exports = router;
