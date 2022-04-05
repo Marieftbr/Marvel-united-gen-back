376f2d319abd00a202834148982392e5709b1c6f
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-04-05 11:14:16 +0200

    add CRUD for box router

--- routes/box.js
+++ routes/box.js
@@ -4,10 +4,25 @@ const cloudinary = require("cloudinary").v2;
 
 const Box = require("../models/Box");
 
-router.get("/box", (req, res) => {
-  res.json({ message: "Box List" });
+//route for read a list of boxes
+router.get("/box", async (req, res) => {
+  try {
+    const boxes = await Box.find();
+    res.json(
+      boxes.map((box) => {
+        return {
+          _id: box._id,
+          name: box.name,
+          // picture: picture.url,
+        };
+      })
+    );
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// route for create box
 router.post("/box", async (req, res) => {
   try {
     const pictureToUpload = req.files.picture.path;
@@ -34,6 +49,43 @@ router.post("/box", async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+//route for update box
+router.put("/box", async (req, res) => {
+  try {
+    if (req.fields.id) {
+      const box = await Box.findById(req.fields.id);
+      const pictureToUpload = req.files.picture.path;
+
+      box.name = req.fields.name;
+      await box.save();
+      box.picture = await cloudinary.uploader.upload(pictureToUpload, {
+        folder: `marvelUnited/box/${box._id}`,
+      });
+
+      await box.save();
+      res.json(box);
+    } else {
+      res.status(400).json({ message: "Missing parameter" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+//route for delete a box
+router.delete("/box", async (req, res) => {
+  try {
+    if (req.fields.id) {
+      await Box.findByIdAndDelete(req.fields.id);
+      res.json({ message: "Box is deleted" });
+    } else {
+      res.status(400).json({ message: "Missing id" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
